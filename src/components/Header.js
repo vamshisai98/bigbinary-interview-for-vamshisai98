3682eb0ef7d534aaaacc6fa40610c37d3881d454
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-03-19 16:48:52 +0530

    SpaceX logo implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import logo from './logo.png';
 
 const Header = () => {
-  return <div className='header'>SpaceX</div>;
+  return (
+    <div className='header'>
+      <img src={logo} alt='' style={{ width: '300px' }} />
+    </div>
+  );
 };
 
 export default Header;
