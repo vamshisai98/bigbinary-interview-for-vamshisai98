d718e25dc3690b0aa94b1b7f3e08ea4f7f281aa7
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-03-21 21:11:49 +0530

    mac os link bug update

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Filter = ({ setFilter, filter, toValue, fromValue, filterValue }) => {
+  const history = useHistory();
+
   const handleChangeFilter = (e) => {
     e.preventDefault();
     setFilter(e.target.value);
+    history.push(`/${1}/${e.target.value}/${fromValue}/${toValue}`);
   };
 
   return (
@@ -13,13 +16,14 @@ const Filter = ({ setFilter, filter, toValue, fromValue, filterValue }) => {
         className='fas fa-filter'
         style={{ paddingTop: '8px', fontSize: '11px' }}
       ></i>
-      {filter && <Redirect to={`/${1}/${filter}/${fromValue}/${toValue}`} />}
+      {/* <Link to={`/${1}/${filter}/${fromValue}/${toValue}`}> */}
       <select onChange={handleChangeFilter} defaultValue={filterValue}>
         <option value='all'>All Launches</option>
         <option value='upcoming'>Upcoming Launches</option>
         <option value='success'>Successful Launches</option>
         <option value='failed'>Failed Launches</option>
       </select>
+      {/* </Link> */}
     </div>
   );
 };
