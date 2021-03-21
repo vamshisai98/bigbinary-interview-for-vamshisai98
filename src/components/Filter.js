47b4cc73deedaa7eab8588e130a5bac3df9152ce
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-03-21 20:48:09 +0530

    fixed mac os link bug

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const Filter = ({ setFilter, filter, toValue, fromValue, filterValue }) => {
   const handleChangeFilter = (e) => {
@@ -13,14 +13,13 @@ const Filter = ({ setFilter, filter, toValue, fromValue, filterValue }) => {
         className='fas fa-filter'
         style={{ paddingTop: '8px', fontSize: '11px' }}
       ></i>
-      <Link to={`/${1}/${filter}/${fromValue}/${toValue}`}>
-        <select onChange={handleChangeFilter} defaultValue={filterValue}>
-          <option value='all'>All Launches</option>
-          <option value='upcoming'>Upcoming Launches</option>
-          <option value='success'>Successful Launches</option>
-          <option value='failed'>Failed Launches</option>
-        </select>
-      </Link>
+      {filter && <Redirect to={`/${1}/${filter}/${fromValue}/${toValue}`} />}
+      <select onChange={handleChangeFilter} defaultValue={filterValue}>
+        <option value='all'>All Launches</option>
+        <option value='upcoming'>Upcoming Launches</option>
+        <option value='success'>Successful Launches</option>
+        <option value='failed'>Failed Launches</option>
+      </select>
     </div>
   );
 };
